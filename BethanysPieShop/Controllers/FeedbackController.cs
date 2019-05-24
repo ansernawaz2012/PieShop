9da6196ffa45700c34c4db99947e3ac404510b35
ansernawaz2012 <ansernawaz2012@gmail.com>
2019-05-24 15:37:00 +0100

    Add project files.

---
+++ BethanysPieShop/Controllers/FeedbackController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BethanysPieShop.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        private readonly IFeedbackRepository _feedbackRepository;
+
+        public FeedbackRepository(IFeedbackRepository feedbackRepository)
+        {
+            _feedbackRepository = feedbackRepository;
+
+
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Feedback feedback)
+        {
+            _feedbackRepository.AddFeedback(feedback);
+            return RedirectToAction("FeedbackComplete");
+        }
+
+        public IActionResult FeedbackComplete()
+        {
+            return View();
+        }
+    }
+}
